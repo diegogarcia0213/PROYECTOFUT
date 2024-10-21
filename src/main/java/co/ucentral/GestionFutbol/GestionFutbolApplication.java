7ca62ce9709c1dc1b2c16363bd638ee77f612f09
Diego Garcia <dgarciam8@ucentral.edu.co>
2024-10-20 20:45:46 -0500

    (nuevo): estructura

---
+++ src/main/java/co/ucentral/GestionFutbol/GestionFutbolApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.GestionFutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFutbolApplication.class, args);
+	}
+
+}
